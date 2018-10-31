340cba131815c95a68d0bbb88dcb39c81ed5f60e
wvicioso <wellington.vicioso@gmail.com>
2018-10-31 05:35:17 -0400

    Updates

---
+++ precache-manifest.3919cd0610bad2914373df9da74ab226.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "ba93efd3e8e5bb8c72498eb0e1ac4c40",
+    "url": "/static/media/tls-camera.ba93efd3.jpg"
+  },
+  {
+    "revision": "d2b3583ce669f828404669c5e6cb21e7",
+    "url": "/static/media/private.d2b3583c.jpg"
+  },
+  {
+    "revision": "0f9e552836cf4a6307c584287e7ab8d3",
+    "url": "/static/media/TLSLogo.0f9e5528.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "70b7559960cc793194df",
+    "url": "/static/js/main.70b75599.chunk.js"
+  },
+  {
+    "revision": "ed13315c264758d62c17",
+    "url": "/static/js/1.ed13315c.chunk.js"
+  },
+  {
+    "revision": "70b7559960cc793194df",
+    "url": "/static/css/main.c5cd436b.chunk.css"
+  },
+  {
+    "revision": "93d217d74577c7caebb70637025c0363",
+    "url": "/index.html"
+  }
+];
